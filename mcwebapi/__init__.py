fa7717700d96253991aadfc90efda809ac8f77c8
addavriance <61160742+addavriance@users.noreply.github.com>
2025-11-03 13:29:41 +0300

    Update __init__.py

--- mcwebapi/__init__.py
+++ mcwebapi/__init__.py
@@ -4,18 +4,10 @@ A Python client for interacting with Minecraft servers via WebSocket API.
 """
 
 from .api import MinecraftAPI
-from .core import MinecraftClient, Promise
-from .objects import Player, Level, Command, Block
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __author__ = "addavriance"
 
 __all__ = [
-    "MinecraftAPI",
-    "MinecraftClient",
-    "Promise",
-    "Player",
-    "Level",
-    "Command",
-    "Block",
+    "MinecraftAPI"
 ]
