33a6b19008d552a8812aedfe8aa559d234ca84a6
addavriance <61160742+addavriance@users.noreply.github.com>
2025-11-03 13:33:18 +0300

    feat: added support for static argument sets

--- mcwebapi/objects/level.py
+++ mcwebapi/objects/level.py
@@ -5,5 +5,5 @@ from ..api import MinecraftClient
 class Level(SocketInstance):
     """Level object for interacting with world-related operations"""
 
-    def __init__(self, client: MinecraftClient):
-        super().__init__("level", client)
+    def __init__(self, client: MinecraftClient, identifier: str):
+        super().__init__("level", client, identifier)
